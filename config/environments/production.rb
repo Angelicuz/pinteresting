b32321d03c599496bb0c7517fca4b9e0dbfacdf2
Angel Ramos <angel@emaginelab.com>
2015-03-04 18:13:31 -0500

    Implemented Devise for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #Note to set this to your actual host
+  #Required for heroku
+  config.action_mailer.default_url_options = { host: => 'http://angel-test-app.herokuapp.com' }
 end
